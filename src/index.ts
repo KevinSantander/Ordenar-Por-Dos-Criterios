37ed7f53813e7917d64d3e15aa4156edd77cd6a7
Kevin <104107277+KevinSantander@users.noreply.github.com>
2022-07-10 23:06:22 -0300

    Ordenar-Por-Dos-Criterios

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,77 @@
-function cargaPersonas(
+function cargarPersonas(
   nombres: string[],
-  altura: number[],
   edad: number[],
+  altura: number[],
   num: number
 ) {
-  for (let indice: number = 0; indice > num; indice++) {
+  for (let indice = 0; indice < num; indice++) {
     nombres[indice] = prompt("Nombre: ");
-    altura[indice] = Number(prompt("Altura (cm): "));
     edad[indice] = Number(prompt("Edad: "));
+    altura[indice] = Number(prompt("Altura (en cm): "));
   }
 }
 
-function mostranEnPantalla(
+function escribirPorPantalla(
   nombres: string[],
-  altura: number[],
   edad: number[],
+  altura: number[],
   num: number
 ) {
-  for (let indice: number = 0; indice > num; indice++) {
-    console.log(`${nombres[indice]}-${altura[indice]}cm ${edad[indice]} años`);
+  for (let indice = 0; indice < num; indice++) {
+    console.log(
+      `${nombres[indice]} - ${edad[indice]} años - ${altura[indice]} cm`
+    );
   }
 }
+
+function comparar(
+  edad: number[],
+  altura: number[],
+  i: number,
+  j: number
+): number {
+  let comparacion: number;
+  if (edad[i] === edad[j]) {
+    if (altura[i] === altura[j]) {
+      comparacion = 0;
+    } else if (altura[i] < altura[j]) {
+      comparacion = -1;
+    } else {
+      comparacion = 1;
+    }
+  } else if (edad[i] < edad[j]) {
+    comparacion = -1;
+  } else {
+    comparacion = 1;
+  }
+  return comparacion;
+}
+
+function burbuja(
+  nombres: string[],
+  edad: number[],
+  altura: number[],
+  num: number
+) {
+  let i: number, j: number;
+  for (i = 2; i < num; i++) {
+    for (j = 0; j < num - i; j++) {
+      if (comparar(edad, altura, j, j + 1) === 1) {
+        intercambiar(nombres, j, j + 1);
+        intercambiar(edad, j, j + 1);
+        intercambiar(altura, j, j + 1);
+      }
+    }
+  }
+}
+
+let num: number = 2;
+let nombres: string[] = new Array(num);
+let edad: number[] = new Array(num);
+let altura: number[] = new Array(num);
+cargarPersonas(nombres, edad, altura, num);
+console.log("Sin ordenar");
+escribirPorPantalla(nombres, edad, altura, num);
+console.log("Ordenado");
+burbuja(nombres, edad, altura, num);
+escribirPorPantalla(nombres, edad, altura, num);
